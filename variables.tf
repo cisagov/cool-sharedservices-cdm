1095bbe6c11634de5a1edcaaaa727b182cad9f27
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-02-02 10:58:39 -0500

    Modify TF code to create the s2s VPN tunnel to the VENOM environment

--- variables.tf
+++ variables.tf
@@ -1,39 +1,34 @@
 # ------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
+# Required parameters
 #
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "subnet_id" {
+variable "venom_vpn_preshared_key" {
   type        = string
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)"
+  description = "The pre-shared key to use for setting up the site-to-site VPN connection between the COOL and VENOM..  This must be a string of 36 characters, which can include alphanumerics, periods, and underscores (e.g. \"abcdefghijklmnopqrstuvwxyz0123456789\")."
 }
 
 # ------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
+# Optional parameters
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
-variable "ami_owner_account_id" {
-  type        = string
-  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
-  default     = "self"
-}
-
-variable "aws_availability_zone" {
-  type        = string
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
-  default     = "a"
-}
 
 variable "aws_region" {
   type        = string
-  description = "The AWS region to deploy into (e.g. us-east-1)"
+  description = "The AWS region where the shared services account is to be created (e.g. \"us-east-1\")."
   default     = "us-east-1"
+}
+
+variable "provisionaccount_role_name" {
+  type        = string
+  description = "The name of the IAM role that allows sufficient permissions to provision all AWS resources in the Shared Services account."
+  default     = "ProvisionAccount"
 }
 
 variable "tags" {
   type        = map(string)
-  description = "Tags to apply to all AWS resources created"
+  description = "Tags to apply to all AWS resources created."
   default     = {}
 }
