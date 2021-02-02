c28baee25e09ce96ee6741a84272bd77557f8c24
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-02-02 11:17:49 -0500

    Update README for this repo
    Also remove an unnecessary variable and fix a typo in a variable
    description.

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@
 
 variable "venom_vpn_preshared_key" {
   type        = string
-  description = "The pre-shared key to use for setting up the site-to-site VPN connection between the COOL and VENOM..  This must be a string of 36 characters, which can include alphanumerics, periods, and underscores (e.g. \"abcdefghijklmnopqrstuvwxyz0123456789\")."
+  description = "The pre-shared key to use for setting up the site-to-site VPN connection between the COOL and VENOM.  This must be a string of 36 characters, which can include alphanumerics, periods, and underscores (e.g. \"abcdefghijklmnopqrstuvwxyz0123456789\")."
 }
 
 # ------------------------------------------------------------------------------
@@ -19,12 +19,6 @@ variable "aws_region" {
   type        = string
   description = "The AWS region where the shared services account is to be created (e.g. \"us-east-1\")."
   default     = "us-east-1"
-}
-
-variable "provisionaccount_role_name" {
-  type        = string
-  description = "The name of the IAM role that allows sufficient permissions to provision all AWS resources in the Shared Services account."
-  default     = "ProvisionAccount"
 }
 
 variable "tags" {
