be0cb617c46f24a005c523fcbdfa430b1f55df67
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-02-08 14:55:54 -0500

    Add newly-created resources as outputs
    This is in case we need to reference them elsewhere.

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "venom_customer_gateway" {
   description = "The gateway for the site-to-site VPN connection to VENOM."
 }
 
+output "venom_tgw_route_table" {
+  value       = aws_ec2_transit_gateway_route_table.venom
+  description = "The custom Transit Gateway route table for the VENOM VPN connection."
+}
+
+output "venom_tgw_route_table_association" {
+  value       = aws_ec2_transit_gateway_route_table_association.venom
+  description = "The association between the VENOM VPN connection and its custom Transit Gateway route table."
+}
+
 output "venom_vpn_connection" {
   value       = aws_vpn_connection.venom
   description = "The site-to-site VPN connection to VENOM."
