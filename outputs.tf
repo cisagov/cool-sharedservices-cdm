748e9ec786d79e716c815e62923fe2925782e163
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-02-09 17:52:30 -0500

    Add the security group and prefix list to the outputs

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "venom_customer_gateway" {
   description = "The gateway for the site-to-site VPN connection to VENOM."
 }
 
+output "venom_prefix_list" {
+  value       = aws_ec2_managed_prefix_list.venom
+  description = "A prefix list for the VENOM CIDRs."
+}
+
+output "venom_security_group" {
+  value       = aws_security_group.venom
+  description = "A security group that allows for all necessary communications between the VENOM agents and the VENOM CIDRs."
+}
+
 output "venom_tgw_route_table" {
   value       = aws_ec2_transit_gateway_route_table.venom
   description = "The custom Transit Gateway route table for the VENOM VPN connection."
