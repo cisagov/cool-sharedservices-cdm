47b0711516681e07810617737b8aea81e118f245
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-04-16 16:25:13 -0400

    Modify VPN config to restart the tunnel if we detect a dead peer
    This should restart the connection if the other side of the tunnel
    dies on us.

--- s2s_vpn_tunnel.tf
+++ s2s_vpn_tunnel.tf
@@ -39,8 +39,11 @@ resource "aws_vpn_connection" "cdm" {
       "Name" = "CDM site-to-site VPN connection"
     },
   )
-  transit_gateway_id                   = data.terraform_remote_state.networking.outputs.transit_gateway.id
-  tunnel_inside_ip_version             = "ipv4"
+  transit_gateway_id       = data.terraform_remote_state.networking.outputs.transit_gateway.id
+  tunnel_inside_ip_version = "ipv4"
+  # Try to reconnect when we detect that the other side of the tunnel
+  # is dead
+  tunnel1_dpd_timeout_action           = "restart"
   tunnel1_ike_versions                 = ["ikev2"]
   tunnel1_phase1_dh_group_numbers      = [14]
   tunnel1_phase1_encryption_algorithms = ["AES256"]
@@ -51,6 +54,9 @@ resource "aws_vpn_connection" "cdm" {
   tunnel1_phase2_integrity_algorithms  = ["SHA2-384"]
   tunnel1_phase2_lifetime_seconds      = 3600
   tunnel1_preshared_key                = var.cdm_vpn_preshared_key
+  # Try to reconnect when we detect that the other side of the tunnel
+  # is dead
+  tunnel2_dpd_timeout_action           = "restart"
   tunnel2_ike_versions                 = ["ikev2"]
   tunnel2_phase1_dh_group_numbers      = [14]
   tunnel2_phase1_encryption_algorithms = ["AES256"]
